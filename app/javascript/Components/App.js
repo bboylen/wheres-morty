30618c8b4d5211e3c35266472142edf702db34f7
Brian <bbbrianboylen@gmail.com>
2021-03-31 22:15:45 -0500

    Tried to setup layout styling

--- app/javascript/Components/App.js
+++ app/javascript/Components/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import GameDisplay from './GameDisplay';
+import InfoDisplay from './InfoDisplay';
 
 const App = (props) => {
   return (
     <div id="app">
+      <InfoDisplay />
       <GameDisplay />
+      
     </div>
   )
 }
---
+++ app/javascript/Components/InfoDisplay.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const InfoDisplay = (props) => {
+  return (
+    <div id="info-display">
+      <div id="character-info">
+       <h1 >Characters:</h1>
+       <div id="character-list">
+         <div class="character">
+           
+         </div>
+       </div>
+      </div>
+      
+      <h1>Time:</h1>
+    </div>
+  )
+}
+
+export default InfoDisplay;
