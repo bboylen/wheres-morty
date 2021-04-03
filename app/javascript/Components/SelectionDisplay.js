d4f60b435a0d256b9f52ff388ea56ced281d2158
Brian <bbbrianboylen@gmail.com>
2021-04-03 16:35:16 -0500

    added select box

--- app/javascript/Components/SelectionDisplay.js
+++ app/javascript/Components/SelectionDisplay.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const SelectionDisplay = (props) => {
+  console.log(props.locationSelected[0]);
+  let horizontalOffset =
+    props.locationSelected[0] / window.outerWidth > 0.5 ? -190 : 60;
   return (
     <div id="selection-display">
-      <div id="selection-target" style={{left: props.locationSelected[0]-62, top: props.locationSelected[1]-62}} ></div>
-      <ul id="selection-list" style={{left: props.locationSelected[0]+32, top: props.locationSelected[1]+32}}>
+      <div
+        id="selection-target"
+        style={{
+          left: props.locationSelected[0] - 62,
+          top: props.locationSelected[1] - 62,
+        }}
+      ></div>
+      <ul
+        id="selection-list"
+        style={{
+          left: props.locationSelected[0] + horizontalOffset,
+          top: props.locationSelected[1] + 32,
+        }}
+      >
         <li>Amish Cyborg</li>
         <li>Pencilvester</li>
         <li>Mini Cowboy</li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
 export default SelectionDisplay;
