194b72454a565ca970d07151adad06d9d8771fb4
Brian <bbbrianboylen@gmail.com>
2021-04-17 17:51:21 -0500

    Switched to postgres db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_03_222237) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "characters", force: :cascade do |t|
     t.integer "xcoordinate"
     t.integer "ycoordinate"
